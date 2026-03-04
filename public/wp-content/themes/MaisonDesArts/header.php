d54fe04edf735ff709f3aa84bd8c59c3e7ffbe69
Cassiopée Gariépy <2442173@csfoy.ca>
2026-03-04 16:21:01 -0500

    test avec css

--- public/wp-content/themes/MaisonDesArts/header.php
+++ public/wp-content/themes/MaisonDesArts/header.php
@@ -12,7 +12,8 @@
 
 <meta charset='<?php bloginfo('charset');?>'/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/liaisons/css/styles.css"/>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/liaisons/css/styles.css"/>  ici ancienne version
+<!-- <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/public/wp-content/themes/MaisonDesArts/liaisons/css/styles.css"/>  ici test -->
 <?php wp_head(); ?>
 </head>
 <body>
