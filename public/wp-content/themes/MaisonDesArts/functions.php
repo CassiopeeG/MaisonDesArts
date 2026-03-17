e8d167d4011afa9f0382b35761aee794d94a644a
Cassiopée Gariépy <2442173@csfoy.ca>
2026-03-17 08:31:58 -0400

    Modifié fichier fonctions

--- public/wp-content/themes/MaisonDesArts/functions.php
+++ public/wp-content/themes/MaisonDesArts/functions.php
@@ -9,100 +9,51 @@ if(function_exists('register_nav_menus')){
 ?>
 
 <?php
-//Déclaration du type d'article personnalisé des réalisations****************************
+//Déclaration du type d'article personnalisé des Volets ****************************
 //À mettre dans le fichier functions.php et adapter en fonction du type d'article.
 
-//Déclaration des Réalisations
-function agence_realisations_custom_post() {
+//Déclaration des Volets
+function maison_des_arts_volets_custom_post() {
 
     //On rentre les différentes dénominations de notre article personnalisé type
     //qui seront affichées dans l'interface administrative...
     $labels = array(
         // Le nom au pluriel
-        'name'                => _x( 'Réalisations de Mon Agence', 'Post Type General Name'),
+        'name'                => _x( 'Volets Culturels de la Maison des arts', 'Post Type General Name'),
         // Le nom au singulier
-        'singular_name'       => _x( 'Réalisations', 'Post Type Singular Name'),
+        'singular_name'       => _x( 'Volets Culturels', 'Post Type Singular Name'),
         // Le libellé affiché dans le menu
-        'menu_name'           => __( 'Réalisations'),
+        'menu_name'           => __( 'Volets Culturels'),
         //Les différents libellés de l'interface administrative
-        'all_items'           => __( 'Toutes nos réalisations'),
-        'view_item'           => __( 'Voir nos réalisations'),
-        'add_new_item'        => __( 'Ajouter une nouvelle réalisation'),
+        'all_items'           => __( 'Tout les volets culturels'),
+        'view_item'           => __( 'Voir nos volets culturels'),
+        'add_new_item'        => __( 'Ajouter un nouveau volet'),
         'add_new'             => __( 'Ajouter'),
-        'edit_item'           => __( 'Editer une réalisation'),
-        'update_item'         => __( 'Modifier une réalisation'),
-        'search_items'        => __( 'Rechercher une réalisation'),
+        'edit_item'           => __( 'Editer un volet'),
+        'update_item'         => __( 'Modifier un volet'),
+        'search_items'        => __( 'Rechercher un volet'),
         'not_found'           => __( 'Non trouvé'),
         'not_found_in_trash'  => __( 'Non trouvé dans la corbeille')
     );
 
     //On peut définir ici d'autres options pour notre type d'article personnalisé
     $args = array(
-        'label'               => __( 'Nos réalisations'),
-        'description'         => __( 'Tous sur nos réalisations'),
+        'label'               => __( 'Nos Volets Culturels'),
+        'description'         => __( 'Tous sur nos Volets Culturels'),
         'labels'              => $labels,
         'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail',
             'comments', 'revisions', 'custom-fields'),
         'hierarchical'        => false,
         'public'              => true,
         'has_archive'         => true,
-        'rewrite'			  => array( 'slug' => 'realisations')
+        'rewrite'			  => array( 'slug' => 'volets')
     );
 
     // On enregistre notre type d'article personnalisé qu'on nomme ici "realisations" et ses arguments
-    register_post_type( 'realisations', $args );
+    register_post_type( 'volets', $args );
 }
 
-add_action( 'init', 'agence_realisations_custom_post', 0 );
-?>
-
-
-<?php
-//Déclaration du type d'article personnalisé des réalisations****************************
-//À mettre dans le fichier functions.php et adapter en fonction du type d'article.
-
-//Déclaration des Réalisations
-function agence_services_custom_post() {
-
-    //On rentre les différentes dénominations de notre article personnalisé type
-    //qui seront affichées dans l'interface administrative...
-    $labels = array(
-        // Le nom au pluriel
-        'name'                => _x( 'Services de Mon Agence', 'Post Type General Name'),
-        // Le nom au singulier
-        'singular_name'       => _x( 'Services', 'Post Type Singular Name'),
-        // Le libellé affiché dans le menu
-        'menu_name'           => __( 'Services'),
-        //Les différents libellés de l'interface administrative
-        'all_items'           => __( 'Toutes nos services'),
-        'view_item'           => __( 'Voir nos services'),
-        'add_new_item'        => __( 'Ajouter un nouveau services'),
-        'add_new'             => __( 'Ajouter'),
-        'edit_item'           => __( 'Editer un services'),
-        'update_item'         => __( 'Modifier un services'),
-        'search_items'        => __( 'Rechercher un services'),
-        'not_found'           => __( 'Non trouvé'),
-        'not_found_in_trash'  => __( 'Non trouvé dans la corbeille')
-    );
-
-    //On peut définir ici d'autres options pour notre type d'article personnalisé
-    $args = array(
-        'label'               => __( 'Nos services'),
-        'description'         => __( 'Tous sur nos services'),
-        'labels'              => $labels,
-        'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail',
-            'comments', 'revisions', 'custom-fields'),
-        'hierarchical'        => false,
-        'public'              => true,
-        'has_archive'         => true,
-        'rewrite'			  => array( 'slug' => 'services')
-    );
-
-    // On enregistre notre type d'article personnalisé qu'on nomme ici "realisations" et ses arguments
-    register_post_type( 'services', $args );
-}
-
-add_action( 'init', 'agence_services_custom_post', 0 );
+add_action( 'init', 'maison_des_arts_volets_custom_post', 0 );
 ?>
 
 <?php
