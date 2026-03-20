c214b194daf73fcc2b661b50d3c23e32d1cc9bcb
Alice Tremblay <2461132@csfoy.ca>
2026-03-20 10:53:42 -0400

    correction image

--- public/wp-content/themes/MaisonDesArts/page-nouvelles.php
+++ public/wp-content/themes/MaisonDesArts/page-nouvelles.php
@@ -45,7 +45,7 @@ if($posts){
 
             <?php if(has_post_thumbnail()){ ?>
                 <div class="article__imageUne">
-                    <?php the_post_thumbnail("small"); ?>
+                    <?php the_post_thumbnail("image-article"); ?>
                 </div>
             <?php } ?>
         </article>
