0512e83a231b024f1969cda558f608829033ce3a
awaly <2348627@csfoy.ca>
2026-03-23 14:44:28 -0400

    Sauvegarde avant pull

--- public/wp-content/themes/MaisonDesArts/page-volets.php
+++ public/wp-content/themes/MaisonDesArts/page-volets.php
@@ -1,17 +1,50 @@
 <?php
 /* Template Name: Volets */
 get_header();
+
+$page_id = get_queried_object_id();
+$citation = get_field('citation_volets', $page_id);
+$texte_intro = get_field('texte_intro_volets', $page_id);
+$image_hero = get_field('image_hero_volets', $page_id);
 ?>
 
-<main class="page_volets">
+<main class="volets_page">
 
-    <div>
-        <h2 class= ' titre_h2'> Un lieu culturel complet : nos 4 volets </h2>
+    <h2 class="volets_page_titre">Un lieu culturel <br> complet : nos 4 volets</h2>
+
+    <section class="volets_mise_en_avant">
+
+        <div class="volets_mise_en_avant_texte">
+            <div class="volets_citation">
+                <span class="volets_citation_guillemet_ouverture">❝</span>
+
+                <div class="volets_citation_contenu">
+                    <?php echo $citation; ?>
+                </div>
+
+                <span class="volets_citation_guillemet_fermeture">❞</span>
+            </div>
+        </div>
+
+        <div class="volets_mise_en_avant_image">
+            <?php if ($image_hero) { ?>
+                <div class="volets_image_hero">
+                    <img src="<?php echo $image_hero['url']; ?>" alt="<?php echo $image_hero['alt']; ?>">
+                </div>
+            <?php } ?>
+        </div>
+
+    </section>
+
+    <div class="volets_separateur">
+        <p>Voici ce qui vous attend :</p>
     </div>
 
-    <p>
-        <?php the_content(); ?>
-    </p>
+    <?php if ($texte_intro) { ?>
+        <div class="volets_texte_intro">
+            <?php echo $texte_intro; ?>
+        </div>
+    <?php } ?>
 
     <?php
     $posts = get_posts(array(
@@ -27,33 +60,41 @@ get_header();
         <?php foreach ($posts as $post) { ?>
             <?php setup_postdata($post); ?>
 
-            <article class="article">
-                <header class="article__entete">
-                    <h3 class="article__titre">
-                        <a class="article__lien" href="<?php the_permalink(); ?>">
+            <article class="volets_bloc">
+
+                <?php
+                $image_info = get_field('photo_1', get_the_ID());
+                if ($image_info) { ?>
+                    <div class="volets_bloc_image">
+                        <img src="<?php echo $image_info['url']; ?>" alt="<?php echo $image_info['alt']; ?>">
+                    </div>
+                <?php } ?>
+
+                <header class="volets_bloc_entete">
+                    <h3 class="volets_bloc_titre">
+                        <a class="volets_bloc_lien" href="<?php the_permalink(); ?>">
                             <?php the_title(); ?>
                         </a>
                     </h3>
                 </header>
 
-                <?php
-                $image_info = get_field("photo_1", get_the_ID());
-                if ($image_info) { ?>
-                    <div class="article__imageUne">
-                        <img src="<?php echo $image_info['url']; ?>" alt="<?php echo $image_info['alt']; ?>">
-                    </div>
-                <?php } ?>
+             <div class="volets_bloc_texte">
+    <p class="volets_bloc_resume">
+        <?php
+        $texte = get_field('texte_volet', get_the_ID());
+        echo wp_trim_words(strip_tags($texte), 25, '...');
+        ?>
+    </p>
 
-                <p class="article__texte">
-                    <?php
-                    $texte = get_field("texte_volet", get_the_ID());
-                    echo wp_trim_words(strip_tags($texte), 25, '...');
-                    ?>
-                </p>
+    <p class="volets_bloc_date">
+        <?php echo get_field('date_volet', get_the_ID()); ?>
+    </p>
 
-                <p class="article__date">
-                    <?php echo get_field("date_volet", get_the_ID()); ?>
-                </p>
+    <a class="volets_bloc_lire" href="<?php the_permalink(); ?>">
+        Lire l’article complet →
+    </a>
+</div>
+
             </article>
 
         <?php } ?>
