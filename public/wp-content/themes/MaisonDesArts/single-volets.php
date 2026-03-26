164a7b5f8a2c9ca2e000d3f6221bdb4401ac2f63
awaly <2348627@csfoy.ca>
2026-03-26 18:29:59 -0400

    resolve conflict remove css map

--- public/wp-content/themes/MaisonDesArts/single-volets.php
+++ public/wp-content/themes/MaisonDesArts/single-volets.php
@@ -2,35 +2,74 @@
 get_header();
 ?>
 
-<main class="page">
+<main class="single_volets_page">
 
     <?php the_post(); ?>
-    <article class="article">
-        <header class="article__entete">
-            <h2 class="article__titre"><?php the_title() ?></h2>
+
+    <?php
+    $image_info = get_field('photo_1', get_the_ID());
+    $texte_volet = get_field('texte_volet', get_the_ID());
+    $date_volet = get_field('date_volet', get_the_ID());
+
+    $date_affichee = '';
+
+    if ($date_volet) {
+        $date_obj = DateTime::createFromFormat('Ymd', $date_volet);
+
+        if ($date_obj) {
+            $date_affichee = date_i18n('j F Y', $date_obj->getTimestamp());
+        } else {
+            $date_affichee = $date_volet;
+        }
+    }
+    ?>
+
+    <article class="single_volets_article">
+
+        <header class="single_volets_entete">
+            <h1 class="page-header"><?php the_title(); ?></h1>
         </header>
 
-        <?php
-        $image_info = get_field("photo_1", get_the_ID());
 
-        if($image_info != null){
-        ?>
-            <picture>
-                <source media="(min-width: 800px)" srcset="<?= $image_info['sizes']['large'] ?? $image_info['url'] ?>">
-                <source media="(min-width: 601px)" srcset="<?= $image_info['sizes']['medium'] ?? $image_info['url'] ?>">
-                <img src="<?= $image_info['sizes']['thumbnail'] ?? $image_info['url'] ?>" alt="<?= $image_info['alt']; ?>">
-            </picture>
-        <?php } ?>
+        <section class="single_volets_visuel">
 
-        <p class="article__texte">
-            <?php echo get_field("texte_volet", get_the_ID()); ?>
-        </p>
+            <?php if ($image_info) { ?>
+                <div class="single_volets_image">
+                    <img src="<?php echo $image_info['url']; ?>" alt="<?php echo $image_info['alt']; ?>">
+                </div>
+            <?php } ?>
 
-        <footer class="article__piedPage">
-            <h4>Publié le: <?php echo get_field("date_volet", get_the_ID()); ?></h4>
-        </footer>
+            <div class="single_volets_bloc_bleu">
+                <div class="single_volets_bloc_contenu">
+
+                    <?php if ($texte_volet) { ?>
+                        <div class="single_volets_texte">
+                            <?php echo $texte_volet; ?>
+                        </div>
+                    <?php } ?>
+
+                    <?php if ($date_affichee) { ?>
+                        <div class="single_volets_date">
+                            <?php echo $date_affichee; ?>
+                        </div>
+                    <?php } ?>
+
+                </div>
+            </div>
+
+        </section>
+
+        <nav class="single_volets_navigation">
+            <?php
+            the_post_navigation(array(
+                'prev_text' => '← Consulter l’article précédent',
+                'next_text' => 'Consulter l’article suivant →'
+            ));
+            ?>
+        </nav>
+
     </article>
 
 </main>
 
-<?php get_footer() ?>
+<?php get_footer(); ?>
