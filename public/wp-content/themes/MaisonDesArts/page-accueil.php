d35551e62421908dfd145b8abae8f9e78df97c9c
Cassiopée Gariépy <2442173@csfoy.ca>
2026-03-31 08:24:09 -0400

    mise à jour des cartes page accueil

--- public/wp-content/themes/MaisonDesArts/page-accueil.php
+++ public/wp-content/themes/MaisonDesArts/page-accueil.php
@@ -26,43 +26,52 @@ get_header(); //Appel de l'inclusion d'entête de page
             </h3>
         </div>
             
-        <div class="container-articles"><?php
-        $posts = get_posts(array(
-            'posts_per_page' => 2,
-            'post_type' => 'post',
-            'post_status' => 'publish',
-            'orderby' => 'date',
-            'order' => 'DESC',
-            ));
-            
-        if($posts){
-            foreach($posts as $post){
-                setup_postdata($post); ?>
-
-                <article class="article">
+        <div class="container-articles">
+            <?php
+            $posts = get_posts(array(
+                'posts_per_page' => 2,
+                'post_type' => 'post',
+                'post_status' => 'publish',
+                'orderby' => 'date',
+                'order' => 'DESC',
+                ));
+                
+            if($posts){
+                foreach($posts as $post){
+                    setup_postdata($post); ?>
+                        
+            <article class="article">
+                <div class="article__padding">
                     <?php if(has_post_thumbnail()){ ?>
-                        <div class="article__imageUne">
-                            <?php the_post_thumbnail("image-article"); ?>
-                        </div><?php 
-                    } ?>
-            
-                    <header class="article__entente">
-                        <h2 class="article_titre">
-                            <?php the_title(); ?>
-                        </h2>
-                    </header>
-                
-                    <p class="article__texte">
-                        <?php the_excerpt(); ?>
-                    </p>
-                
-                    <p>Consulter l'article
-                        <a class="article__lien" href="<?php the_permalink();?>"></a>
-                    </p>
-                </article><?php }
-            wp_reset_postdata();
-        }?>
-        </div>   
+                    <div class="article__imageUne-Carte">
+                    <?php the_post_thumbnail("image-article"); ?>
+                </div>
+            <?php } ?>
+
+            <header class="article__entente">
+                <h2 class="article_titre">
+                        <?php the_title(); ?>
+                </h2>
+             <div class="article__date--div">
+            <h4 class="article__date"><?php the_date(); //Attention! Nécessite un appel à the_post() avant cet affichage ?></h4>
+        </div>
+        </header>
+
+        <p class="article__texte">
+            <?php the_excerpt(); ?>
+        </p>
+        
+        <p>
+            <a class="article__lien" href="<?php the_permalink();?>">
+                Consulter l'article →
+            </a>
+        </p>
+    </div>
+    </article>
+
+    <?php }
+    wp_reset_postdata();}?>
+    </div>
     </section>
 
     <section class="volets">
