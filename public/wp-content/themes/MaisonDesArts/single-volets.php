1c79eb7f24f05f70e52278584938bdfbcc5a5ab8
awaly <2348627@csfoy.ca>
2026-04-02 17:59:38 -0400

    dernier push

--- public/wp-content/themes/MaisonDesArts/single-volets.php
+++ public/wp-content/themes/MaisonDesArts/single-volets.php
@@ -1,103 +1,72 @@
 <?php
 get_header();
-
-$page_courante = isset($_GET['paged']) ? (int) $_GET['paged'] : 1;
-$articles_par_page = 2;
-
-$posts = get_posts(array(
-    'posts_per_page' => $articles_par_page,
-    'post_type'      => 'volets',
-    'post_status'    => 'publish',
-    'orderby'        => 'menu_order title',
-    'order'          => 'ASC',
-    'paged'          => $page_courante
-));
-
-$total_posts = wp_count_posts('volets')->publish;
-$total_pages = ceil($total_posts / $articles_par_page);
 ?>
 
-<main class="single_volets_page"  id="contenu">
+<main class="single_volets_page" id="contenu">
 
-    <?php if ($posts) { ?>
-        <?php foreach ($posts as $post) { ?>
-            <?php setup_postdata($post); ?>
+    <?php the_post(); ?>
+
+    <?php
+    $image_info = get_field('photo_1', get_the_ID());
+    $texte_volet = get_field('texte_volet', get_the_ID());
+    $date_volet = get_field('date_volet', get_the_ID());
+
+    $date_affichee = '';
+
+    if ($date_volet) {
+        $date_obj = DateTime::createFromFormat('Ymd', $date_volet);
+
+        if ($date_obj) {
+            $date_affichee = date_i18n('j F Y', $date_obj->getTimestamp());
+        } else {
+            $date_affichee = $date_volet;
+        }
+    }
+    ?>
+
+    <article class="single_volets_article">
+
+        <header class="single_volets_entete">
             <h1 class="page-header"><?php the_title(); ?></h1>
+        </header>
 
-            <?php
-            $image_info = get_field('photo_1', get_the_ID());
-            $texte_volet = get_field('texte_volet', get_the_ID());
-            $date_volet = get_field('date_volet', get_the_ID());
+        <section class="single_volets_visuel">
 
-            $date_affichee = '';
+            <?php if ($image_info) { ?>
+                <div class="single_volets_image">
+                    <img src="<?php echo $image_info['url']; ?>" alt="<?php echo $image_info['alt']; ?>">
+                </div>
+            <?php } ?>
 
-            if ($date_volet) {
-                $date_obj = DateTime::createFromFormat('Ymd', $date_volet);
+            <div class="single_volets_bloc_bleu">
+                <div class="single_volets_bloc_contenu">
 
-                if ($date_obj) {
-                    $date_affichee = date_i18n('j F Y', $date_obj->getTimestamp());
-                } else {
-                    $date_affichee = $date_volet;
-                }
-            }
-            ?>
-
-            <article class="single_volets_article">
-
-                <header class="single_volets_entete">
-                    
-                </header>
-
-                <section class="single_volets_visuel">
-
-                    <?php if ($image_info) { ?>
-                        <div class="single_volets_image">
-                            <img src="<?php echo $image_info['url']; ?>" alt="<?php echo $image_info['alt']; ?>">
+                    <?php if ($texte_volet) { ?>
+                        <div class="single_volets_texte">
+                            <?php echo $texte_volet; ?>
                         </div>
                     <?php } ?>
 
-                    <div class="single_volets_bloc_bleu">
-                        <div class="single_volets_bloc_contenu">
-
-                            <?php if ($texte_volet) { ?>
-                                <div class="single_volets_texte">
-                                    <?php echo $texte_volet; ?>
-                                </div>
-                            <?php } ?>
-
-                            <?php if ($date_affichee) { ?>
-                                <div class="single_volets_date">
-                                    <?php echo $date_affichee; ?>
-                                </div>
-                            <?php } ?>
-
+                    <?php if ($date_affichee) { ?>
+                        <div class="single_volets_date">
+                            <?php echo $date_affichee; ?>
                         </div>
-                    </div>
+                    <?php } ?>
 
-                </section>
+                </div>
+            </div>
 
-            </article>
+        </section>
 
-        <?php } ?>
-        <?php wp_reset_postdata(); ?>
-    <?php } ?>
+    </article>
 
     <nav class="single_volets_navigation">
-        <div class="nav-links">
-
-            <div class="nav-previous">
-                <?php if ($page_courante > 1) { ?>
-                    <a href="?paged=<?php echo $page_courante - 1; ?>">← Consulter l’article précédent</a>
-                <?php } ?>
-            </div>
-
-            <div class="nav-next">
-                <?php if ($page_courante < $total_pages) { ?>
-                    <a href="?paged=<?php echo $page_courante + 1; ?>">Consulter l’article suivant →</a>
-                <?php } ?>
-            </div>
-
-        </div>
+        <?php
+        the_post_navigation(array(
+            'prev_text' => '← %title',
+            'next_text' => '%title →',
+        ));
+        ?>
     </nav>
 
 </main>
